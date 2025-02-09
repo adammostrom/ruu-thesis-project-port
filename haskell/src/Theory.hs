a5df66654f419a564ccbd8aed4d5da459d4885c4
Adam Mos <moztrom93@gmail.com>
2025-02-10 00:04:31 +0100

    started on Theory implementation, added documentation

---
+++ haskell/src/Theory.hs
@@ -0,0 +1,12 @@
+module Theory where 
+
+import Rel.TotalPreorder
+
+{-
+Theory consists of two sets of components:
+ - One for the specification of sequential decision problems (SDPs)
+ - One for their solution with verified backward induction
+
+-}
+
+
