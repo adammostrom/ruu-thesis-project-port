dc5019bd6474fa01bbd85bc119222bb89a0b9481
davidek0 <ek200302@gmail.com>
2025-04-14 12:07:00 +0200

    Refactor 20250414 lunch

---
+++ haskell/src/Probabilities.hs
@@ -0,0 +1,74 @@
+module Probabilities(..) where
+
+
+pS_Start :: Double
+pS_Start = 0.9
+
+pD_Start :: Double
+pD_Start = (1.0 - pS_Start)
+
+pD_Delay :: Double
+pD_Delay = 0.9
+
+pS_Delay :: Double
+pS_Delay = (1.0 - pD_Delay)
+
+pL_S_DH :: Double
+pL_S_DH = 0.7
+
+pL_S_DL :: Double
+pL_S_DL = 0.9
+
+pL_S_SL :: Double
+pL_S_SL = 0.7
+
+pL_S_SH :: Double
+pL_S_SH = 0.3
+
+pL_D_DH :: Double
+pL_D_DH = pL_S_SH
+
+pL_D_DL :: Double
+pL_D_DL = (pL_S_SL)
+
+pU_S_0 :: Double
+pU_S_0 = 0.9
+
+pU_D_0 :: Double
+pU_D_0 = 0.7
+
+pU_S :: Double
+pU_S = 0.9
+
+pU_D :: Double
+pU_D = 0.3
+
+pH_S_DH :: Double
+pH_S_DH = (1.0 - pL_S_DH)
+
+pH_S_SH :: Double
+pH_S_SH = (1.0 - pL_S_SH)
+
+pH_S_DL :: Double
+pH_S_DL = (1.0 - pL_S_DL)
+
+pH_S_SL :: Double
+pH_S_SL = (1.0 - pL_S_SL)
+
+pH_D_DH :: Double
+pH_D_DH = (1.0 - pL_D_DH)
+
+pH_D_DL :: Double
+pH_D_DL = (1.0 - pL_D_DL)
+
+pC_S_0 :: Double
+pC_S_0 = (1.0 - pU_S_0)
+
+pC_D_0 :: Double
+pC_D_0 = (1.0 - pU_D_0)
+
+pC_S :: Double
+pC_S = (1.0 - pU_S)
+
+pC_D :: Double
+pC_D = (1.0 - pU_D)
--- haskell/src/Theory.hs
+++ haskell/src/Theory.hs
@@ -5,135 +5,235 @@ module Theory where
 -- NOTE: Idris code marked in comments as beginning with ">".
 
 import Control.Monad (liftM2)
+import Probabilities
 
-
--- Probability distribution type (uncertainty monad)
-newtype Dist a = Dist {getDist :: [(a, Double)]} deriving (Show, Eq)
-
-newtype Val = Val Double deriving (Show, Eq, Ord)
-
-type Nat = Int
+--------------------------------------------------------------------
+-- Theory.hs
 type Time = Int
-type Horizon = Int
 
--- Policy Type
-{-
-> Policy : (t : Nat) -> Type
-> Policy t = (x : X t) -> Y t x
--}
-type Policy = State -> Action
-
-{-
->data PolicySeq : (t : Nat) -> (n : Nat) -> Type where
-    >   Nil   :  {t : Nat} -> PolicySeq t Z
-    >   (::)  :  {t, n : Nat} -> Policy t -> PolicySeq (S t) n -> PolicySeq t (S n)
--}
-data PolicySeq = Nil | Cons Policy PolicySeq
+type Policy = Map
 
 
-instance Functor Dist where
-  fmap f (Dist xs) = Dist [(f x, p) | (x, p) <- xs]
 
-instance Applicative Dist where
-  pure x = Dist [(x, 1.0)]
-  Dist fs <*> Dist xs = Dist [(f x, p1 * p2) | (f, p1) <- fs, (x, p2) <- xs]
+data State = Nil | State deriving (Show, Eq, Ord)
 
-instance Monad Dist where
-  return = pure
-  Dist xs >>= f = Dist [(y, p * p') | (x, p) <- xs, let Dist ys = f x, (y, p') <- ys]
 
--- Define Actions
--- Defined actions here instead of Specifications.hs
-data Action = Start | Delay | Unit
-  deriving (Show, Eq, Enum)
+data Control t x = Control State x deriving (Show)
 
--- States as defined from the paper, moved state from Specification.hs to Theory.hs
-data State = DHU | DHC | DLU | DLC | SHU | SHC | SLU | SLC
-  deriving (Show, Eq, Enum)
+-- Available controls: given a time & state, return a list of valid controls
+availableControls ::  Time -> State -> [Control]
 
--- Example of a Policy
-ctrl :: Policy -- State -> Action
-ctrl DHU = Start
-ctrl DHC = Delay
-ctrl DLU = Unit
-ctrl DLC = Unit
-ctrl SHU = Start
-ctrl SHC = Delay
-ctrl SLU = Unit
-ctrl SLC = Unit
-
--- Successor function
-newtype S t = S t
-
--- Next state transition
-{-  > next : (t : Nat) -> (x : X t) -> Y t x -> M (X (S t)) -}
--- Transition function (we will mock the transition probabilities based on actions)
-nextT :: State -> Action -> Dist State
-nextT DHU Start = Dist [(DHC, 0.9), (DLU, 0.1)] -- Example, can modify based on your actual logic
-
-    -- This is a strange example: deciding to start the transition
-    -- should lead with at least a non-zero probability to some state
-    -- with "S" as the first letter.
-
-nextT DHC Delay = Dist [(DHU, 0.8), (DHC, 0.2)]
-nextT _ _ = Dist [(DHU, 1.0)] -- Default transition
 
 -- Reward function
-{- > reward  :  (t : Nat) -> (x : X t) -> Y t x -> X (S t) -> Val -}
-reward :: State -> Action -> State -> Val
-reward SHU _ _ = Val 1
-reward _ _ _ = Val 0
+reward :: X a => a -> Control a b -> a -> Float
 
--- Measurement function
-{- > meas :  M Val -> Val -}
--- Measurement function (combines values with their probabilities)
-meas :: Dist Val -> Val
-meas (Dist xs) = Val (sum [v * p | (Val v, p) <- xs])
+--------------------------------------------------------------------
+-- Inititalize states
+DHU :: State
+DHC :: State
+DLU :: State
+DLC :: State
+SHU :: State
+SHC :: State
+SLU :: State
+SLC :: State
+
+Start :: Control
+Delay :: Control
+Unity :: Control
+
+isCommitted :: State -> Bool
+isCommitted x = True if x == DHC ||  x == DLC  ||  x == SHC ||  x == SLC else False
+
+isDisrupted :: State -> Bool
+isDisrupted x = True if x == DLU ||  x == DLC  ||  x == SLU ||  x == SLC else False
+
+isStarted :: State -> Bool
+isStarted x = True if x == SHU ||  x == SHC  ||  x == SLU ||  x == SLC else False
 
 
+availableControls _ x = Unity if isStarted == True    -- Irreversibility of choice -> sink
+availableControls t x = [Start, Delay]                -- For now ignore time.
 
--- Value function for policy sequences
-{-
-> val : Functor M => {t, n : Nat} -> PolicySeq t n -> X t -> Val
-    > val {t}  Nil      x  =  zero
-    > val {t} (p :: ps) x  =  let y    =  p x in
-    >                         let mx'  =  next t x y in
-    >                         meas (map (reward t x y <++> val ps) mx')
--}
--- Value function (recursive calculation)
-val :: PolicySeq -> State -> Dist Val   -- PJ: why Dist Val and not just Val?
-val Nil _ = return (Val 0)
-val (Cons p ps)  x = do
-    let y = p x
-    mx' <- nextT x y
-    Dist [(Val v, p) | (Val v, p) <- getDist (fmap (\x' -> addVal (reward x y x') (val ps x')) mx')]
-      -- strange deviation from the Idris code (perhaps due to wrong type?)
+reward :: Time -> State -> Control -> State -> Val
+reward _ _ _ x' = if x' == DHU || x' == SHU then 1 
+                else 0
 
-addVal :: Val -> Dist Val -> Dist Val
-addVal (Val v1) (Dist xs) = Dist [(Val (v1 + v2), p) | (Val v2, p) <- xs]
--- Why do you want to define addition for this assymetric type?
 
--- Placeholder for best and worst policy extensions
-{-
-> bestExt  :  Functor M => {t, n : Nat} -> PolicySeq (S t) n -> Policy t
--}
--- Function to compute the best action given a policy sequence
-bestExt :: PolicySeq -> Policy
-bestExt _ = ctrl -- Placeholder, for now returns the ctrl function for each state
+next :: Time -> State -> Control -> Prob [(State, Probability)]
+next 0 DHU Start = mkProbabilityList
+  [ (DHU, pD_Start * pH_D_DH * pU_D_0),
+    (DHC, pD_Start * pH_D_DH * pC_D_0),
+    (DLU, pD_Start * pL_D_DH * pU_D_0),
+    (DLC, pD_Start * pL_D_DH * pC_D_0),
+    (SHU, pS_Start * pH_S_DH * pU_S_0),
+    (SHC, pS_Start * pH_S_DH * pC_S_0),
+    (SLU, pS_Start * pL_S_DH * pU_S_0),
+    (SLC, pS_Start * pL_S_DH * pC_S_0)
+  ]
+next 0 DHU Delay = mkProbabilityList
+  [ (DHU, pD_Delay * pH_D_DH * pU_D_0),
+    (DHC, pD_Delay * pH_D_DH * pC_D_0),
+    (DLU, pD_Delay * pL_D_DH * pU_D_0),
+    (DLC, pD_Delay * pL_D_DH * pC_D_0),
+    (SHU, pS_Delay * pH_S_DH * pU_S_0),
+    (SHC, pS_Delay * pH_S_DH * pC_S_0),
+    (SLU, pS_Delay * pL_S_DH * pU_S_0),
+    (SLC, pS_Delay * pL_S_DH * pC_S_0)
+  ]
+next 0 DHC Start = mkProbabilityList
+  [ (DHC, pD_Start * pH_D_DH),
+    (DLC, pD_Start * pL_D_DH),
+    (SHC, pS_Start * pH_S_DH),
+    (SLC, pS_Start * pL_S_DH)
+  ]
+next 0 DHC Delay = mkProbabilityList
+  [ (DHC, pD_Delay * pH_D_DH),
+    (DLC, pD_Delay * pL_D_DH),
+    (SHC, pS_Delay * pH_S_DH),
+    (SLC, pS_Delay * pL_S_DH)
+  ]
+next 0 DLU Start = mkProbabilityList
+  [ (DHU, pD_Start * pH_D_DL * pU_D_0),
+    (DHC, pD_Start * pH_D_DL * pC_D_0),
+    (DLU, pD_Start * pL_D_DL * pU_D_0),
+    (DLC, pD_Start * pL_D_DL * pC_D_0),
+    (SHU, pS_Start * pH_S_DL * pU_S_0),
+    (SHC, pS_Start * pH_S_DL * pC_S_0),
+    (SLU, pS_Start * pL_S_DL * pU_S_0),
+    (SLC, pS_Start * pL_S_DL * pC_S_0)
+  ]
+next 0 DLU Delay = mkProbabilityList
+  [ (DHU, pD_Delay * pH_D_DL * pU_D_0),
+    (DHC, pD_Delay * pH_D_DL * pC_D_0),
+    (DLU, pD_Delay * pL_D_DL * pU_D_0),
+    (DLC, pD_Delay * pL_D_DL * pC_D_0),
+    (SHU, pS_Delay * pH_S_DL * pU_S_0),
+    (SHC, pS_Delay * pH_S_DL * pC_S_0),
+    (SLU, pS_Delay * pL_S_DL * pU_S_0),
+    (SLC, pS_Delay * pL_S_DL * pC_S_0)
+  ]
+next 0 DLC Start = mkProbabilityList
+  [ (DHC, pD_Start * pH_D_DL),
+    (DLC, pD_Start * pL_D_DL),
+    (SHC, pS_Start * pH_S_DL),
+    (SLC, pS_Start * pL_S_DL)
+  ]
+next 0 DLC Delay = mkProbabilityList
+  [ (DHC, pD_Delay * pH_D_DL),
+    (DLC, pD_Delay * pL_D_DL),
+    (SHC, pS_Delay * pH_S_DL),
+    (SLC, pS_Delay * pL_S_DL)
+  ]
+next 0 SHU _ = mkProbabilityList
+  [ (SHU, pH_S_SH * pU_S_0),
+    (SHC, pH_S_SH * pC_S_0),
+    (SLU, pL_S_SH * pU_S_0),
+    (SLC, pL_S_SH * pC_S_0)
+  ]
+next 0 SHC _ = mkProbabilityList
+  [ (SHC, pH_S_SH),
+    (SLC, pL_S_SH)
+  ]
+next 0 SLU _ = mkProbabilityList
+  [ (SHU, pH_S_SL * pU_S_0),
+    (SHC, pH_S_SL * pC_S_0),
+    (SLU, pL_S_SL * pU_S_0),
+    (SLC, pL_S_SL * pC_S_0)
+  ]
+next 0 SLC _ = mkProbabilityList
+  [ (SHC, pH_S_SL),
+    (SLC, pL_S_SL)
+  ]
+next t DHU Start = mkProbabilityList
+  [ (DHU, pD_Start * pH_D_DH * pU_D),
+    (DHC, pD_Start * pH_D_DH * pC_D),
+    (DLU, pD_Start * pL_D_DH * pU_D),
+    (DLC, pD_Start * pL_D_DH * pC_D),
+    (SHU, pS_Start * pH_S_DH * pU_S),
+    (SHC, pS_Start * pH_S_DH * pC_S),
+    (SLU, pS_Start * pL_S_DH * pU_S),
+    (SLC, pS_Start * pL_S_DH * pC_S)
+  ]
+next t DHU Delay = mkProbabilityList
+  [ (DHU, pD_Delay * pH_D_DH * pU_D),
+    (DHC, pD_Delay * pH_D_DH * pC_D),
+    (DLU, pD_Delay * pL_D_DH * pU_D),
+    (DLC, pD_Delay * pL_D_DH * pC_D),
+    (SHU, pS_Delay * pH_S_DH * pU_S),
+    (SHC, pS_Delay * pH_S_DH * pC_S),
+    (SLU, pS_Delay * pL_S_DH * pU_S),
+    (SLC, pS_Delay * pL_S_DH * pC_S)
+  ]
+next t DHC Start = mkProbabilityList
+  [ (DHC, pD_Start * pH_D_DH),
+    (DLC, pD_Start * pL_D_DH),
+    (SHC, pS_Start * pH_S_DH),
+    (SLC, pS_Start * pL_S_DH)
+  ]
+next t DHC Delay = mkProbabilityList
+  [ (DHC, pD_Delay * pH_D_DH),
+    (DLC, pD_Delay * pL_D_DH),
+    (SHC, pS_Delay * pH_S_DH),
+    (SLC, pS_Delay * pL_S_DH)
+  ]
+next t DLU Start = mkProbabilityList
+  [ (DHU, pD_Start * pH_D_DL * pU_D),
+    (DHC, pD_Start * pH_D_DL * pC_D),
+    (DLU, pD_Start * pL_D_DL * pU_D),
+    (DLC, pD_Start * pL_D_DL * pC_D),
+    (SHU, pS_Start * pH_S_DL * pU_S),
+    (SHC, pS_Start * pH_S_DL * pC_S),
+    (SLU, pS_Start * pL_S_DL * pU_S),
+    (SLC, pS_Start * pL_S_DL * pC_S)
+  ]
+next t DLU Delay = mkProbabilityList
+  [ (DHU, pD_Delay * pH_D_DL * pU_D),
+    (DHC, pD_Delay * pH_D_DL * pC_D),
+    (DLU, pD_Delay * pL_D_DL * pU_D),
+    (DLC, pD_Delay * pL_D_DL * pC_D),
+    (SHU, pS_Delay * pH_S_DL * pU_S),
+    (SHC, pS_Delay * pH_S_DL * pC_S),
+    (SLU, pS_Delay * pL_S_DL * pU_S),
+    (SLC, pS_Delay * pL_S_DL * pC_S)
+  ]
+next t DLC Start = mkProbabilityList
+  [ (DHC, pD_Start * pH_D_DL),
+    (DLC, pD_Start * pL_D_DL),
+    (SHC, pS_Start * pH_S_DL),
+    (SLC, pS_Start * pL_S_DL)
+  ]
+next t DLC Delay = mkProbabilityList
+  [ (DHC, pD_Delay * pH_D_DL),
+    (DLC, pD_Delay * pL_D_DL),
+    (SHC, pS_Delay * pH_S_DL),
+    (SLC, pS_Delay * pL_S_DL)
+  ]
+next t SHU Unity = mkProbabilityList
+  [ (SHU, pH_S_SH * pU_S),
+    (SHC, pH_S_SH * pC_S),
+    (SLU, pL_S_SH * pU_S),
+    (SLC, pL_S_SH * pC_S)
+  ]
+next t SHC Unity = mkProbabilityList
+  [ (SHC, pH_S_SH),
+    (SLC, pL_S_SH)
+  ]
+next t SLU Unity = mkProbabilityList
+  [ (SHU, pH_S_SL * pU_S),
+    (SHC, pH_S_SL * pC_S),
+    (SLU, pL_S_SL * pU_S),
+    (SLC, pL_S_SL * pC_S)
+  ]
+next t SLC Unity = mkProbabilityList
+  [ (SHC, pH_S_SL),
+    (SLC, pL_S_SL)
+  ]
+next _ _ _ = error "Invalid state or action combination"
 
-{-
-> bestExtSpec  :  Functor M => {t, n : Nat} ->
->                 (ps : PolicySeq (S t) n) -> BestExt ps (bestExt ps)
--}
+type Network = [Prob [State]]
 
-worstExt :: PolicySeq -> Policy
-worstExt _ = ctrl -- Placeholder
-
--- Extract head and tail of policy sequences
-headPolicy :: PolicySeq -> Maybe Policy
-headPolicy Nil = Nothing
-headPolicy (Cons p _) = Just p
-
-tailPolicy :: PolicySeq -> Maybe PolicySeq
-tailPolicy Nil = Nothing
-tailPolicy (Cons _ ps) = Just ps
+bi :: RewardFnc -> Network -> (Reward, [State])
+bi reward network = foldr network combine leafValue network
+    where
+        leafValue :: Network -> (Reward, )
