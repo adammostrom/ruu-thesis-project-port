3a77e498dbdc11ab5bf0a586fafa31749b6f0e90
Adam Mos <moztrom93@gmail.com>
2025-04-15 21:53:39 +0200

    some more testing, also added Theory model which does not use uncertainty monad

--- haskell/tests/test_specific/Test_matterMost.hs
+++ haskell/tests/test_specific/Test_matterMost.hs
@@ -1,16 +1,14 @@
 module Test_matterMost where
 
-
-import Theory hiding (State, Action)
 import Specification
 import Test.QuickCheck
-
+import Theory hiding (Action, State)
 
 instance Arbitrary State where
-    arbitrary = elements [DHU, DHC, DLU, DLC, SHU, SHC, SLU, SLC] 
+  arbitrary = elements [DHU, DHC, DLU, DLC, SHU, SHC, SLU, SLC]
 
 instance Arbitrary Action where
-    arbitrary = elements [Start, Delay, Unit] 
+  arbitrary = elements [Start, Delay, Unit]
 
 ----------------- Unit Tests ---------------------------------------------------
 {-
@@ -20,95 +18,54 @@ instance Arbitrary Action where
     "Horizon, best, value:  1,  Delay,  0.468"
 -}
 unit_best :: Bool
-unit_best = 
-    let result = bestWrap 0 1 DHU
-    in fst result == Delay && snd result >= 0.467 && snd result <= 0.469
-
+unit_best =
+  let result = bestWrap 0 1 DHU
+   in fst result == Delay && snd result >= 0.467 && snd result <= 0.469
 
 ----------------- Property Tests -----------------------------------------------
+
+-- Tests that the policy we get for bestExt is "better" than that of worstExt
 prop_best_geq_worst :: State -> Property
 prop_best_geq_worst s =
-  forAll (choose (1, 5)) $ \h ->
+  forAll (choose (1, 8)) $ \h ->
     let ps = biWrap 0 h
         bestPol = bestExtWrap 0 ps
         worstPol = worstExtWrap 0 ps
 
-        bestVal = valWrap (bestPol : ps) s
-        worstVal = valWrap (worstPol : ps) s
-
-    in bestVal >= worstVal
+        bestVal = valWrap 0 (bestPol : ps) s
+        worstVal = valWrap 0 (worstPol : ps) s
+     in bestVal >= worstVal
 
 -- Val returns positive values
-prop_val_non_negative ::  State -> Property
-prop_val_non_negative s = forAll (choose (1, 5)) $ \h -> 
-    let ps = biWrap 0 h
-        v  = valWrap 0 ps s 
-    in v >= 0
+prop_val_non_negative :: State -> Property
+prop_val_non_negative s = forAll (choose (1, 8)) $ \h ->
+  let ps = biWrap 0 h
+      v = valWrap 0 ps s
+   in v >= 0
 
 -- best functions returns valid actions for that state
 prop_best_gives_valid_action :: State -> Property
-prop_best_gives_valid_action s = 
-    forAll (choose (1, 5)) $ \h ->
-        let (a,_) = bestWrap 0 h s
-            validActions = actions h s
-        in a `elem` validActions
-{- 
-prop_best_worst :: Int -> Int -> Bool
-prop_best_worst t n =
-    let ps = bi t n
-        best = [bestExtWrap t ps]
-        worst = [worstExtWrap t ps]
-    in val t ps (fst best) >= val t ps (fst worst)
- -}
+prop_best_gives_valid_action s =
+  forAll (choose (1, 5)) $ \h ->
+    let (a, _) = bestWrap 0 h s
+        validActions = actions h s
+     in a `elem` validActions
+
+-- Test that the state provided by next is subsequent state of the previous state
+prop_deterministic_next :: Int -> PolicySeq State Action -> State -> Property
+prop_deterministic_next t (p:ps) x = 
+    case Map.lookup x p of
+        Nothing -> property True
+        Just y ->
+            expected = reward t x y x' + val (t + 1) ps x'
+            in val t (p:ps) x === expected
 
 
 
-
-
--- A function to run any property
-run_tests :: Testable prop => String -> prop -> IO ()
-run_tests name test = do
-    putStrLn $ "Running test: " ++ name
-    quickCheck test
-
--- Concrete runner for your specific prop
-run_prop_val_non_negative :: IO ()
-run_prop_val_non_negative = run_tests "prop_val_non_negative" prop_val_non_negative
-
-
-
-
-main :: IO()
-main = do
-    quickCheck prop_val_non_negative
-    quickCheck prop_best_gives_valid_action
-
-
-
-
-
-
-{- 
--- Utility function to report test results
-runTest :: String -> Bool -> IO ()
-runTest name result =
-    putStrLn $ if result
-        then "[PASS] " ++ name 
-        else "[FAIL] " ++ name
-
--- Running unit tests
-runUnitTests :: IO ()
-runUnitTests = do
-    putStrLn "Running Unit Tests..."
-    runTest "testReward_DHU" testReward_SHU_SLC
-
--- Running QuickCheck property tests
-runQuickCheckTests :: IO ()
-runQuickCheckTests = do
-    quickCheck prop_reward
-
--- Main function that runs both unit tests and QuickCheck tests
 main :: IO ()
 main = do
-    runUnitTests
-    runQuickCheckTests -}
+  putStrLn $ "Running test: Val return not negative: "
+  quickCheck prop_val_non_negative
+  putStrLn $ "Running test: Best returns a valid action for given state:  "
+  quickCheck prop_best_gives_valid_action
+  quickCheck prop_best_geq_worst
