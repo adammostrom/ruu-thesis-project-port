48ff006db46513a5209cf31b03663184e434d38d
davidek0 <ek200302@gmail.com>
2025-05-10 15:56:50 +0200

    Added memoized refernces in the other SDPCompute funcs

--- haskell/src/SDPCompute.hs
+++ haskell/src/SDPCompute.hs
@@ -18,7 +18,8 @@ val sdp t (p : ps) x =
           recCall x' = reward sdp t x y x' + val sdp (t + 1) ps x'
       in expectation recCall mNext
 
-------------------------------------------------------------------------------------------------------------- | A table mapping each state to its computed value
+-----------------------------------------------------------------------------------------------------------
+-- | A table mapping each state to its computed value
 --   at a given decision step.
 type ValMap x = Map x Val
 
@@ -90,6 +91,13 @@ bi sdp t n =
        p = bestExt sdp t ps_tail
    in  p : ps_tail
 
+bi' :: (Show x, Ord x) => SDP x y -> Int -> Int -> PolicySeq x y
+bi' sdp _ 0 = []
+bi' sdp t n =
+  let  ps_tail = bi' sdp t (n - 1)
+       p = bestExt' sdp t ps_tail
+   in  p : ps_tail
+
 -- Optimal extension
 optExt :: (Show x, Ord x) => (Val->Val->Ordering) -> SDP x y -> Int -> PolicySeq x y -> Policy x y
 optExt cmp sdp t ps = Map.fromList $ map optAction (states sdp t)
@@ -105,9 +113,23 @@ optExt cmp sdp t ps = Map.fromList $ map optAction (states sdp t)
   -- TODO (optional) reuse the computation of val for ps (now
   -- potentially recomputed many times). This should be possible using
   -- memoization.
+optExt' :: (Show x, Ord x) => (Val -> Val -> Ordering) -> SDP x y -> Int -> PolicySeq x y -> Policy x y
+optExt' cmp sdp t ps = Map.fromList $ map optAction (states sdp t)
+  where
+    optAction state = (state, getOptAction t state ps)
+    getOptAction t state ps =
+      let  actionsForState = actions sdp t state
+           actionValues = [  (action, val' sdp t (Map.singleton state action : ps) state)
+                          |  action <- actionsForState
+                          ]
+           opt = maximumBy (cmp `on` snd) actionValues
+      in fst opt
 
 bestExt :: (Show x, Ord x) => SDP x y -> Int -> PolicySeq x y -> Policy x y
 bestExt = optExt compare
+
+bestExt' :: (Show x, Ord x) => SDP x y -> Int -> PolicySeq x y -> Policy x y
+bestExt' = optExt' compare
 
 flipOrder :: Ordering -> Ordering
 flipOrder GT = LT
@@ -117,6 +139,9 @@ flipOrder EQ = EQ
 worstExt :: (Show x, Ord x) => SDP x y -> Int -> PolicySeq x y -> Policy x y
 worstExt = optExt (\x y -> flipOrder (compare x y))
 
+worstExt' :: (Show x, Ord x) => SDP x y -> Int -> PolicySeq x y -> Policy x y
+worstExt' = optExt' (\x y -> flipOrder (compare x y))
+
 best :: (Show x, Ord x) => SDP x y -> Int -> Int -> x -> (y, Val)
 best sdp t n x
     | n == 0 = error "Horizon must be greater than zero!"
@@ -125,6 +150,16 @@ best sdp t n x
              p   = bestExt sdp t ps
              b   = fromMaybe (error "No action found!") (Map.lookup x p)
              vb  = val sdp t (p : ps) x
+        in (b, vb)
+
+best' :: (Show x, Ord x) => SDP x y -> Int -> Int -> x -> (y, Val)
+best' sdp t n x
+    | n == 0 = error "Horizon must be greater than zero!"
+    | otherwise =
+        let  ps  = bi' sdp (t + 1) (n - 1)
+             p   = bestExt' sdp t ps
+             b   = fromMaybe (error "No action found!") (Map.lookup x p)
+             vb  = val' sdp t (p : ps) x
         in (b, vb)
 
 -- Compute how much a state matters for optimal decision-making
