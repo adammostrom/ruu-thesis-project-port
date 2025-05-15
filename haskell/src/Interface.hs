7807ad98ee696558ef4fdcde30770cc13eed2244
nicola <botta@pik-potsdam.de>
2025-05-15 17:14:12 +0200

    Timing 'runBest'.

--- haskell/src/Interface.hs
+++ haskell/src/Interface.hs
@@ -3,6 +3,7 @@ module Interface where
 import Data.Map (Map)
 import Data.Map qualified as Map
 import Prob(Prob, runProb, Probability)
+import Data.Time
 
 -- Import SDP functions
 import SDPCompute
@@ -10,6 +11,14 @@ import SDPCompute
 -- Import the SDP module
 import GHGCase
 
+timeRunBest :: Int -> Int -> State -> IO ()
+timeRunBest decision_step horizon x = do
+   startTime <- getCurrentTime
+   (a, v) <- return (runBest decision_step horizon x)
+   putStrLn $ "(a, v): " ++ (show (a, v))
+   endTime <- getCurrentTime
+   let diff = diffUTCTime endTime startTime
+   putStrLn $ "Execution Time: " ++ (show diff)
 
 runBest :: Int -> Int -> State -> (Action, Val)
 runBest = best ghgcase
